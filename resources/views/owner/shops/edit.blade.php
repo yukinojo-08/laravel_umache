13f07a1eec4dfcfb2dc8aabde819968210d6ad50
yukinojo-08 <engineer.yukinojo@gmail.com>
2023-01-14 22:12:25 +0900

    確認

--- resources/views/owner/shops/edit.blade.php
+++ resources/views/owner/shops/edit.blade.php
@@ -16,7 +16,7 @@
                         <div class="p-2 w-1/2 mx-auto">
                           <div class="relative">
                             <label for="image" class="leading-7 text-sm text-gray-600">画像</label>
-                            <input type="file" id="image" name="image" accept=“image/png,image/jpeg,image/jpg” class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                            <input type="file" id="image" name="image" accept="image/png,image/jpeg,image/jpg" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                           </div>
                         </div>
                         <div class="p-2 w-full flex justify-around mt-4">
