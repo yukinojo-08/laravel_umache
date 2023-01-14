c56876deb65c75b47993787a54493377d3a7896f
yukinojo-08 <engineer.yukinojo@gmail.com>
2023-01-14 22:01:05 +0900

    画像アップロード

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -6,6 +6,10 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Shop;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+use InterventionImage;
+use App\Http\Requests\UploadImageRequest;
+use App\Services\ImageService;
 
 class ShopController extends Controller
 {
@@ -23,7 +27,7 @@ class ShopController extends Controller
                 $shopId = (int)$shopsOwnerId; // キャスト 文字列→数値に型変換
                 $ownerId = Auth::id();
                 if($shopId !== $ownerId){ // 同じでなかったら
-                abort(404); // 404画面表示 }
+                    abort(404); // 404画面表示
                 }
             }
             return $next($request);
@@ -32,18 +36,29 @@ class ShopController extends Controller
 
     public function index()
     {
-        // $ownerId = Auth::id();
+
+        //$ownerId = Auth::id();
         $shops = Shop::where('owner_id', Auth::id())->get();
-        return view('owner.shops.index', compact('shops'));
+
+        return view('owner.shops.index', 
+        compact('shops'));
     }
 
     public function edit($id)
     {
-
+        $shop = Shop::findOrFail($id);
+        // dd(Shop::findOrFail($id));
+        return view('owner.shops.edit', compact('shop'));
     }
 
-    public function update(Request $request, $id)
+    public function update(UploadImageRequest $request, $id)
     {
+        $imageFile = $request->image;
+        if(!is_null($imageFile) && $imageFile->isValid() ){
+            $fileNameToStore = ImageService::upload($imageFile, 'shops');    
+        }
+
+        return redirect()->route('owner.shops.index');
 
     }
 }
