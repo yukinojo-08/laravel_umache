808969de8243689a77b610e6674709c279795f7c
yukinojo-08 <engineer.yukinojo@gmail.com>
2023-01-16 00:43:03 +0900

    image初期設定

--- routes/owner.php
+++ routes/owner.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Owner\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Owner\Auth\RegisteredUserController;
 use App\Http\Controllers\Owner\Auth\VerifyEmailController;
 use App\Http\Controllers\Owner\ShopController;
+use App\Http\Controllers\Owner\ImageController;
 use Illuminate\Support\Facades\Route;
 /*
 |--------------------------------------------------------------------------
@@ -31,6 +32,8 @@ Route::prefix('shops')-> middleware('auth:owners')
         Route::get('edit/{shop}', [shopController::class, 'edit'])->name('shops.edit');
         Route::post('update/{shop}', [shopController::class, 'update'])->name('shops.update');
 });
+
+Route::resource('images', ImageController::class)->middleware('auth:owners')->except(['show']);
 
 Route::get('/dashboard', function () {
     return view('owner.dashboard');
