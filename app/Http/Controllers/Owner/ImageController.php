64d072621e284cce9214c206fa90a72a8f44a4d1
yukinojo-08 <engineer.yukinojo@gmail.com>
2023-01-17 00:05:07 +0900

    image index

--- app/Http/Controllers/Owner/ImageController.php
+++ app/Http/Controllers/Owner/ImageController.php
@@ -3,10 +3,30 @@
 namespace App\Http\Controllers\Owner;
 
 use App\Http\Controllers\Controller;
+use App\Models\Image;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ImageController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth:owners');
+
+        $this->middleware(function ($request, $next) {
+
+            $id = $request->route()->parameter('image');
+            if(!is_null($id)){ // null判定
+            $imagesOwnerId = Image::findOrFail($id)->owner->id;
+                $imageId = (int)$imagesOwnerId; 
+                if($imageId !== Auth::id()){ // 同じでなかったら
+                    abort(404); // 404画面表示
+                }
+            }
+            return $next($request);
+        });
+    } 
     /**
      * Display a listing of the resource.
      *
@@ -14,7 +34,12 @@ class ImageController extends Controller
      */
     public function index()
     {
-        //
+        $images = Image::where('owner_id', Auth::id())
+        ->orderBy('updated_at', 'desc')
+        ->paginate(20);
+
+        return view('owner.images.index', 
+        compact('images'));
     }
 
     /**
