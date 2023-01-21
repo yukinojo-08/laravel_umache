08f161d20cbd301a4fad216b81beb17216cc9f66
yukinojo-08 <engineer.yukinojo@gmail.com>
2023-01-21 22:04:20 +0900

    image seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             AdminSeeder::class,
             OwnerSeeder::class,
-            ShopSeeder::class
+            ShopSeeder::class,
+            ImageSeeder::class
         ]);
     }
 }
